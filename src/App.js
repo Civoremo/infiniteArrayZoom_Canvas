444b465dfa7cb225158a0f83cfbc9e59e968bc68
Civoremo <civoremon@gmail.com>
2020-09-07 20:59:52 -0400

    draw an array on canvas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 /** @format */
 
-import React from "react";
+import React, { createFactory } from "react";
 import "./App.css";
 
 import Canvas from "./canvas";
+
+let columns = 10;
+let rows = 10;
+let grid;
+
+const create2dGrid = () => {
+  const arr = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    arr[i] = new Array(rows);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      arr[i][j] = 0;
+    }
+  }
+  return arr;
+};
+
+grid = create2dGrid();
+
+const draw2dGrid = (ctx) => {
+  let cellSize = ctx.canvas.width / columns;
+
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let x = cellSize * i;
+      let y = cellSize * j;
+
+      ctx.beginPath();
+      ctx.rect(x, y, cellSize, cellSize);
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+      ctx.closePath();
+
+      if (i === 4 && j === 5) {
+        ctx.beginPath();
+        ctx.rect(x, y, cellSize, cellSize);
+        ctx.fillStyle = "darkgreen";
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h2>Canvas Infinite Zoom</h2>
-        <Canvas></Canvas>
+        <Canvas draw2dGrid={draw2dGrid}></Canvas>
       </header>
     </div>
   );
--- src/canvas.js
+++ src/canvas.js
@@ -3,12 +3,14 @@
 import React, { useRef, useEffect } from "react";
 
 const Canvas = (props) => {
-  const {} = props;
+  const {draw2dGrid} = props;
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const cvs = canvasRef.current;
     const ctx = cvs.getContext("2d");
+
+    draw2dGrid(ctx)
 
     return () => {};
   }, []);
